080006f2cbe1236189c42f81394328ef0293c001
yanglang <574748221@qq.com>
2020-04-03 10:54:05 +0800

    bug调整

--- src/views/my/advertiseManage/adList.vue
+++ src/views/my/advertiseManage/adList.vue
@@ -12,6 +12,7 @@
                 v-tap="{methods:tabs,val:item.val}"
                 :class="{'active':nav===item.val}">{{item.name}}
             </li>
+            <li style="display: none">new test</li>
         </ul>
 
         <div class="page-main">
