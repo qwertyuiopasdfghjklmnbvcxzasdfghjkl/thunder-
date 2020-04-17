3d5ac02ef2ec8d2bb164c7a99496016ce786f983
fwan <>
2020-04-17 16:38:58 +0800

    update build ios shell script

--- run_cordova_to_ios.sh
+++ run_cordova_to_ios.sh
@@ -2,7 +2,7 @@
 npm install;
 npm run build-app;
 cordova platform add ios;
-cordova-icon || brew install imagemagick;
+cordova-icon || ((brew install imagemagick) && cordova-icon);
 cordova-splash;
 cordova-hcp build;
 cordova prepare;
