8943011aa9a1b121c1408c8a61e0daa393ee7a7a
15159255754 <2422447171@qq.com>
2020-05-02 11:42:36 +0800

    隐藏 otc 入口

--- src/views/home/index/otc.vue
+++ src/views/home/index/otc.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="otc">
-        <router-link :to="{name:'qotc',params:{type:1}}" class="box">
+      <!--  <router-link :to="{name:'qotc',params:{type:1}}" class="box">
             <label>
                 <p>{{$t('market.flash_buy')}}</p>
                 <span>{{$t('market.flash_buy_small')}}</span>
             </label>
             <img src="../../../assets/img/flash_buy.png"/>
         </router-link>
+        -->
         <router-link :to="{name:'incubation'}" class="box">
             <label>
                 <p>{{$t('incubation.accelerator')}}<!-- 孵息加速器 --></p>
